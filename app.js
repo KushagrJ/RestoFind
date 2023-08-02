e0c4c968c213e1a954e147af1a43e37db3e3734f
KushagrJ <kushagr.j@gmail.com>
2023-08-02 23:55:30 +0530

    Update the Repository

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/resto-find");
 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
 const app = express();
 
 app.set("view engine", "ejs");
